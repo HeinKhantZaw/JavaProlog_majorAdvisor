6d866fa0732113e326fee79243c0305c10ef19f1
Hein Khant Zaw <heinkhantzaw1@gmail.com>
2020-03-09 12:52:53 +0630

    add some testing queries

--- user_input.pl
+++ user_input.pl
@@ -18,6 +18,12 @@ second_first_term_grade(sma, 3).
 second_first_term_grade(b_fis, 3).
 second_first_term_grade(bec, 3).
 second_first_term_grade(elp3, 3).
+second_first_term_grade(osf, 3).
+second_first_term_grade(cal2, 3).
+second_first_term_grade(sma, 3).
+second_first_term_grade(b_fis, 3).
+second_first_term_grade(bec, 3).
+second_first_term_grade(elp3, 3).
 
 :- dynamic second_second_term_grade/2.
 
@@ -31,6 +37,12 @@ second_second_term_grade(dsa, 3).
 second_second_term_grade(ds2, 33).
 second_second_term_grade(hci_is, 3).
 second_second_term_grade(dbms, 33).
+second_second_term_grade(nf, 3).
+second_second_term_grade(elp4, 3).
+second_second_term_grade(dsa, 3).
+second_second_term_grade(ds2, 3).
+second_second_term_grade(hci_is, 3).
+second_second_term_grade(dbms, 3).
 second_second_term_grade(nf, 3).
 second_second_term_grade(elp4, 3).
 second_second_term_grade(dsa, 3).
@@ -60,6 +72,12 @@ third_cs_first_term_grade(an, 3).
 third_cs_first_term_grade(ca, 3).
 third_cs_first_term_grade(sre, 3).
 third_cs_first_term_grade(awt, 3).
+third_cs_first_term_grade(ai, 3).
+third_cs_first_term_grade(em, 3).
+third_cs_first_term_grade(an, 3).
+third_cs_first_term_grade(ca, 3).
+third_cs_first_term_grade(sre, 3).
+third_cs_first_term_grade(awt, 3).
 
 :- dynamic third_cs_second_term_grade/2.
 
@@ -80,6 +98,11 @@ third_cs_second_term_grade(hci_is, 3).
 third_cs_second_term_grade(dbms, 3).
 third_cs_second_term_grade(nf, 3).
 third_cs_second_term_grade(elp4, 3).
+third_cs_second_term_grade(plp, 3).
+third_cs_second_term_grade(co, 3).
+third_cs_second_term_grade(la_ns, 3).
+third_cs_second_term_grade(mp_ee, 3).
+third_cs_second_term_grade(nd_e, 3).
 third_cs_second_term_grade(plp, 3).
 third_cs_second_term_grade(co, 3).
 third_cs_second_term_grade(la_ns, 3).
@@ -114,5 +137,13 @@ interest(ood, 0).
 interest(expert_system, 0).
 interest(algorithms, 1).
 interest(linux_os, 0).
+interest(parallel_computing, 1).
+interest(business, 4).
+interest(analyzing_data, 3).
+interest(programming, 4).
+interest(ood, 2).
+interest(expert_system, 1).
+interest(algorithms, 2).
+interest(linux_os, 1).
 interest(parallel_computing, 1).
 
